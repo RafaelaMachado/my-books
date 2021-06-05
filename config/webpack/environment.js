1bdeb47b1053a71dd3c21a87c55b1ea698341cb5
Rafaela Machado <mmachado.rafaela@gmail.com>
2021-06-05 16:56:48 -0300

    bootstrap setup

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,7 @@
 const { environment } = require('@rails/webpacker')
+const webpack = require('webpack')
+
+environment.plugins.append('Provide', new webpack.ProvidePlugin({
+}))
 
 module.exports = environment
