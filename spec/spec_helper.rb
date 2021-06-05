8b8eaa8762dabf21b93f4d2041cbe287f37f908d
Rafaela Machado <mmachado.rafaela@gmail.com>
2021-06-05 15:36:59 -0300

    creating app with an initial structure, some settings and auxiliary gems

---
+++ spec/spec_helper.rb
@@ -0,0 +1,10 @@
+require 'simplecov'
+
+if ENV['COVERAGE']
+  require 'simplecov'
+  require 'simplecov-rcov'
+  SimpleCov.formatter = SimpleCov::Formatter::RcovFormatter
+  SimpleCov.start 'rails' do
+    add_filter '/spec/'
+  end
+end
