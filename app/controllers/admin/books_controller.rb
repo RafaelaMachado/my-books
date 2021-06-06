154a3158a7e295f5c3472c8c52f50ef0f32261ee
Rafaela Machado <mmachado.rafaela@gmail.com>
2021-06-06 20:24:50 -0300

    Created Books CRUD whith controller, model and views

---
+++ app/controllers/admin/books_controller.rb
@@ -0,0 +1,47 @@
+module Admin
+  class BooksController < BaseController
+    before_action :set_book, only: %i[edit update destroy]
+    
+    def index
+      @search = Book.ransack(params[:q])
+      @books = @search.result
+      respond_with(@books)
+    end
+
+    def new
+      @authors = Author.all.map { |a| [a.name, a.id] }
+      @book = Book.new
+      respond_with(@book)
+    end
+
+    def create
+      @book = Book.create(book_params)
+      respond_with @book, location: -> { admin_books_path }
+    end
+
+    def edit
+      @authors = Author.all.map { |a| [a.name, a.id] }
+      respond_with(@book)
+    end
+
+    def update
+      @book.update(book_params)
+      respond_with @book, location: -> { admin_books_path }
+    end
+
+    def destroy
+      @book.destroy
+      respond_with @book, location: -> { admin_books_path }
+    end
+
+    private
+
+    def book_params
+      params.require(:book).permit(:title, :description, :image, :author_id, :search)
+    end
+
+    def set_book
+      @book = Book.friendly.find(params[:id])
+    end
+  end
+end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,7 @@
 class Book < ApplicationRecord
   belongs_to :author
+  validates :title, presence: true
+  validates :author, presence: true
 
   extend FriendlyId
   friendly_id :title, use: :slugged
